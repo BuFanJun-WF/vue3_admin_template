d021ee8567e708f707007ea10a0341984b0890e3
dwwangfan2 <wangfan_a@aspirecn.com>
2024-02-09 14:08:03 +0800

    feat:环境变量的配置

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+
+console.log(import.meta.env);
 createApp(App).use(ElementPlus).mount("#app");
