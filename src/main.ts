96bff0d5c02be58d6755c596c549a6d81ba0d350
dwwangfan2 <wangfan_a@aspirecn.com>
2024-02-11 23:02:08 +0800

    feat:mock数据

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,18 @@ import "virtual:svg-icons-register";
 import gloablComponent from "@/components/index";
 // 引入模板的全局样式文件
 import "@/styles/index.scss";
+
+// 测试mock接口是否可以使用
+import axios from "axios";
+// 登录接口
+axios({
+  url: "/api/user/login",
+  method: "post",
+  data: {
+    username: "admin",
+    password: "111111",
+  },
+});
 // 这个应该是一个对象，这个对象里面有install方法
 // 只要使用了use方法，就是触发这个组件的install方法
 createApp(App).use(ElementPlus).use(gloablComponent).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -5,34 +5,47 @@ import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import path from "path";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+// mock插件提供方法
+import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
-      symbolId: "icon-[dir]-[name]",
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
-    },
-  },
-  // scss引入全局变量$
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variable.scss";',
+// command用于检测当前的开发环境
+export default defineConfig((command) => {
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+        symbolId: "icon-[dir]-[name]",
+      }),
+      viteMockServe({
+        mockPath: "./mock/", // mock文件所在文件夹
+        //mock接口只是在开发环境的时候使用，这个语句保证开发阶段可以使用mock接口
+        localEnabled: command.command === "serve", // 开发打包开关
+        prodEnabled: command.command !== "serve", // 生产打包开关
+        supportTs: true, // 打开后，可以读取 ts 文件模块。 请注意，打开后将无法监视.js 文件。
+        watchFiles: true, // 监视文件更改
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
       },
     },
-  },
+    // scss引入全局变量$
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variable.scss";',
+        },
+      },
+    },
+  };
 });
