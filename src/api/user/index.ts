40d6d65764869d6b7ed42bc69c30c6271ae8be70
dwwangfan2 <wangfan_a@aspirecn.com>
2024-02-12 11:13:36 +0800

    feat:修改类型名称首字母大写

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,9 +1,9 @@
 // 统一管理咱们项目用户相关的接口
 import request from "@/utils/request";
 import type {
-  loginFormData,
-  loginResponseData,
-  userInfoReponseData,
+  LoginFormData,
+  LoginResponseData,
+  UserInfoReponseData,
 } from "./type";
 // 统一管理接口
 //项目用户相关的请求地址
@@ -15,12 +15,12 @@ enum API {
 // 暴露请求函数
 // 登录接口方法
 // <请求类型，响应类型>
-export const reqLogin = (data: loginFormData) =>
-  request.post<any, loginResponseData>(API.LOGIN_URL, data);
+export const reqLogin = (data: LoginFormData) =>
+  request.post<any, LoginResponseData>(API.LOGIN_URL, data);
 
 // 获取用户信息
 export const reqUserInfo = () =>
-  request.get<any, userInfoReponseData>(API.USERINFO_URL);
+  request.get<any, UserInfoReponseData>(API.USERINFO_URL);
 
 // 退出登录
 export const reqLogout = () => request.post<any, any>(API.LOGOUT_URL);
--- src/api/user/type.ts
+++ src/api/user/type.ts
@@ -1,6 +1,6 @@
 // 定义用户相关数据的ts类型
 // 用户登录接口携带参数的ts类型
-export interface loginFormData {
+export interface LoginFormData {
   username: string;
   password: string;
 }
@@ -13,12 +13,12 @@ export interface ResponseData {
 }
 
 // 定义登录接口返回数据类型
-export interface loginResponseData extends ResponseData {
+export interface LoginResponseData extends ResponseData {
   data: string;
 }
 
 //定义获取用户信息返回数据类型
-export interface userInfoReponseData extends ResponseData {
+export interface UserInfoReponseData extends ResponseData {
   data: {
     routes: string[];
     buttons: string[];
